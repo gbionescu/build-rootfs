e727707963fe0493e48259094dac733b72247c5f
Gabriel Ionescu <plp.github@gmail.com>
2021-07-28 21:52:36 +0300

    enable gh actions on al2 and ubuntu

--- type-amazonlinux2/run-container.sh
+++ type-amazonlinux2/run-container.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cont_id=$(docker run -td --rm  --privileged -v $1:/rootfs.ext4 amazonlinux:2)
 
 # Install systemd
-docker exec -it "$cont_id" /bin/bash -c "yum install -y systemd"
+docker exec -t "$cont_id" /bin/bash -c "yum install -y systemd"
 
 # Restart the container
 docker commit "$cont_id" "temp-build-amazonlinux"
@@ -15,7 +15,7 @@ new_cont_id=$(docker run -td --rm  --privileged -v $1:/rootfs.ext4 temp-build-am
 
 # Run the payload
 set +e
-docker exec -it "$new_cont_id" /bin/sh -c "`cat $SCRIPT_DIR/inside-container.sh`"
+docker exec -t "$new_cont_id" /bin/sh -c "`cat $SCRIPT_DIR/inside-container.sh`"
 
 rval=$?
 set -e
--- type-ubuntu21.04/run-container.sh
+++ type-ubuntu21.04/run-container.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cont_id=$(docker run -td --rm  --privileged -v $1:/rootfs.ext4 ubuntu:21.04)
 
 # Install systemd
-docker exec -it "$cont_id" /bin/bash -c "apt-get update && DEBIAN_FRONTEND='noninteractive' apt-get install -y systemd"
+docker exec -t "$cont_id" /bin/bash -c "apt-get update && DEBIAN_FRONTEND='noninteractive' apt-get install -y systemd"
 
 # Restart the container
 docker commit "$cont_id" "temp-build-ubuntu"
@@ -15,7 +15,7 @@ new_cont_id=$(docker run -td --rm  --privileged -v $1:/rootfs.ext4 temp-build-ub
 
 # Run the payload
 set +e
-docker exec -it "$new_cont_id" /bin/sh -c "`cat $SCRIPT_DIR/inside-container.sh`"
+docker exec -t "$new_cont_id" /bin/sh -c "`cat $SCRIPT_DIR/inside-container.sh`"
 
 rval=$?
 set -e

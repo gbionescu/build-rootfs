83d819aafaa414be0a0f894f3fcd6528e0056b60
Gabriel Ionescu <plp.github@gmail.com>
2021-08-03 17:06:42 +0300

    add sparse parameter

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -3,11 +3,30 @@ set -e
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 RPATH="$SCRIPT_DIR/rootfs.ext4"
 
-if [ $UID != 0 ]; then
-    echo "Needs root"
-    exit 1
-fi
+usage() {
+    echo "\
+Usage: $0 [-t]
+    -s generate sparse file - handling sparse files depends on the rootfs generator.
+"
+}
 
+SPARSE=0 # 1 if creating sparse file
+
+while getopts "h?s" opt; do
+  case "$opt" in
+    h|\?)
+        usage
+        exit 0
+      ;;
+    s)
+        echo "Enabling sparse file creation."
+        SPARSE=1
+        shift
+      ;;
+  esac
+done
+
+# Check if valid parameter
 if [[ ! -d "$SCRIPT_DIR/type-$1" ]]; then
     types=`ls -d1 ./type* | cut -d'-' -f2`
     echo -e "$1 is not a rootfs type. Try one of:\n$types"
@@ -17,6 +36,8 @@ fi
 if [[ -f "$RPATH" ]]; then
     rm -rf "$RPATH"
 fi
+
+export SPARSE="$SPARSE"
 
 "type-$1/init-rootfs.sh" "$RPATH"
 "type-$1/run-container.sh" "$RPATH"
